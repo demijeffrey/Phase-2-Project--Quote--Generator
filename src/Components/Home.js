c62f6e36303e35acee4b740b0b90fc2a27ef9e52
Demaris Ashmore <demiashmore@yahoo.com>
2022-11-01 13:56:20 -0600

    changed text on Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Quote from "./Quote";
+import '../App.css';
 
 function Home({ favClick }) {
 
@@ -13,8 +14,9 @@ function Home({ favClick }) {
 
     return (
         <div>
-            <h2 className="header3">Over 2000 quotes!</h2>
-            <Quote quote={quote} favClick={favClick} />
+            <h2 className="header3">Loaded with over 2000 inspirational quotes!</h2>
+            <br />
+            <Quote quote={quote} favClick={favClick} className="randomQuote" />
         </div>
     )
 }
