ec58a07055284a05592834aa418a6f127f0aa350
Demaris Ashmore <demiashmore@yahoo.com>
2022-11-01 13:33:29 -0600

    import filter

--- src/Components/AllQuotes.js
+++ src/Components/AllQuotes.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Quote from "./Quote";
 import '../App.css';
+import Filter from "./Filter";
 
 function AllQuotes({ favClick }) {
 
@@ -40,13 +41,13 @@ function AllQuotes({ favClick }) {
         <div className="allQuotesContainer">
             <h2 className="header3">All Quotes</h2>
 
-            <select className="filter" onChange={categoryChange}>
+            {/* <select className="filter" onChange={categoryChange}>
                 <option value="" disabled selected>Categories</option>
                 <option value="all">All</option>
                 <option value="love%7Chappiness">Love | Happiness</option>
                 <option value="history%7Ccivil-rights">History | Civil Rights</option>
                 <option value="technology,famous-quotes">Technology | Famous Quotes</option>
-            </select>
+            </select> */}
             <br />
 
             <button className="prevBtn" onClick={last20}>Previous Page</button><button className="nextBtn" onClick={next20}>Next Page</button>
