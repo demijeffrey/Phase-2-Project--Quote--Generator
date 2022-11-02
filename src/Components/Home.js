b76dc778e17ed0cb7c6768c607157b157776a771
Demaris Ashmore <demiashmore@yahoo.com>
2022-11-01 18:39:28 -0600

    added alt to images

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,9 +17,9 @@ function Home({ favClick }) {
             <h2 className="header3">Loaded with over 2000 inspirational quotes!</h2>
             <br />
             <div className="allImages">
-            <img className="image" src="https://viveconstyle.com/wp-content/uploads/2022/01/Start-where-you-are.jpg" />
-            <img className="image2" src="https://graciousquotes.com/wp-content/uploads/2020/02/Short-Inspirational-Quotes-To-Inspire-You-Quickly-Gracious-Quotes-scaled.jpg" />
-            <img className="image3" src="https://t3.ftcdn.net/jpg/02/59/04/18/360_F_259041853_cp094Ywgt7OID8GgociBYZ5gL6KUrgUJ.jpg" />
+            <img className="image" src="https://viveconstyle.com/wp-content/uploads/2022/01/Start-where-you-are.jpg" alt="" />
+            <img className="image2" src="https://graciousquotes.com/wp-content/uploads/2020/02/Short-Inspirational-Quotes-To-Inspire-You-Quickly-Gracious-Quotes-scaled.jpg" alt="" />
+            <img className="image3" src="https://t3.ftcdn.net/jpg/02/59/04/18/360_F_259041853_cp094Ywgt7OID8GgociBYZ5gL6KUrgUJ.jpg" alt="" />
             </div>
             <Quote quote={quote} favClick={favClick} className="randomQuote" />
         </div>
